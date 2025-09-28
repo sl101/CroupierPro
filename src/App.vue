adc5cf410c1f24e1b0f1e8a02cc8c03b0ce437f9
Slava <v.zhevaga@gmail.com>
2025-09-28 15:09:06 +0200

    added roter

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import Count from './views/Count.vue'
+import Roulet from './views/Roulet.vue'
+import NotFound from './views/NotFound.vue'
+
+const routes = {
+  //'/': Home,
+	'/count': Count,
+  '/roulet': Roulet
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+	<h1>Hello world!!!</h1>
+  <a href="#/count">Count</a> |
+  <a href="#/roulet">Roulet</a>
+  <component :is="currentView" />
+</template>	
 
 <style scoped></style>
---
+++ src/views/Count.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+
+	<h1>Count</h1>
+  
+</template>
